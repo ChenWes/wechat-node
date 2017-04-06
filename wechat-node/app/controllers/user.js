b550e071a6d13a022e996f49cc20598b5c38f17b
Wes Chen /GET/IT <ChenWes@esquel.com>
2017-04-06 17:41:31 +0800

    change wechat function
    * get user list from wechat server
    * display user info in local page

--- wechat-node/app/controllers/user.js
+++ wechat-node/app/controllers/user.js
@@ -1,21 +1,39 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
-  User = mongoose.model('user');  
-  
+  User = mongoose.model('user'),
+  wechatAPI = require('wechat-api');
+
+var api = new wechatAPI('wx1434eed5268660c4', '30a5f51682755652e6e02879757a0fb1');
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
+
 router.get('/admin/user', function (req, res, next) {
-  User.find(function (err, users) {
-    // res.json(users);
-    if (err) return next(err);
-    res.render('admin/user', {    
-      title:'WeChat Dashboard - WeChat User',
-      listtitle:'WeChat User List',  
-      Users: users
+  // User.find(function (err, users) {
+  //   if (err) return next(err);
+
+  api.getFollowers((err, result) => {
+    // res.json(result);
+    res.render('admin/user', {
+      title: 'WeChat Dashboard - WeChat User',
+      listtitle: 'WeChat User List',
+      Users: result
+    });
+  });
+
+  // });
+});
+
+router.get('/admin/user/:id', function (req, res, next) {
+  api.getUser({ openid: req.params.id, lang: 'en' }, (err, result) => {
+    // res.json(result);
+    res.render('admin/viewuser', {
+      title: 'WeChat Dashboard - WeChat User',
+      listtitle: 'WeChat User',
+      User: result
     });
   });
 });
